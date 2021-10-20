a9e4084951895863e0434113009490d7131464ce
Natalia Fedorova <nataliafedorova1989@gmail.com>
2021-10-20 17:08:19 +0200

    update

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -4,7 +4,7 @@ export default function ContactItem({ contactInfo }) {
   const iconObject = require(`../assets/icons/${icon}`);
   const iconURL = iconObject.default;
   return (
-    <div>
+    <div id="contact-item">
       <li>
         <a href={href}>
           <img src={iconURL} alt={iconalt} />
