b8f195dc4097ae03e1069e2da8d08d60edbbdffc
Natalia Fedorova <nataliafedorova1989@gmail.com>
2022-05-11 15:42:05 +0200

    add new content and update existing texts, refactor website button

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,11 +1,16 @@
 // Project files
 import JSONProjects from "../data/projects.json";
+import JSONKthProjects from "../data/projects-kth.json";
 import ProjectItem from "../components/ProjectItem";
 import ProjectModal from "../components/ProjectModal";
 
 export default function Projects({ setModal }) {
   // Components
   const ProjectsList = JSONProjects.map((item) => (
+    <ProjectItem key={item.id} item={item} onClick={() => onProject(item)} />
+  ));
+
+  const KTHProjectsList = JSONKthProjects.map((item) => (
     <ProjectItem key={item.id} item={item} onClick={() => onProject(item)} />
   ));
 
@@ -17,16 +22,22 @@ export default function Projects({ setModal }) {
   return (
     <div className="content-section" id="projects">
       <h2>Projects</h2>
+      <h3>KTH</h3>
       <div className="content-grid">
         <div className="grid-left">
           <p>
-            These are the projects I am working on during the Frontend Development 
-            course at Novare Potential. 
+            These are the projects I developed during Software Development Academy at KTH - Royal Instutite of Technology. One individual project and 2 fullstack group projects.
           </p>
+        </div>
+        <div className="grid-left" id="projects-section">
+          {KTHProjectsList}
+        </div>
+      </div>
+      <h3>Novare Potential</h3>
+      <div className="content-grid">
+        <div className="grid-left">
           <p>
-            You can also find projects from my previous course
-            (Software Development Academy at KTH - Royal Instutite of Technology) 
-            on my GitHub page.
+            I worked on following projects during the Frontend Development course at Novare Potential. All of these are my invidiual projects.
           </p>
         </div>
         <div className="grid-left" id="projects-section">
