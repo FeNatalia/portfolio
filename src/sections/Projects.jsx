b45a6a9c7c7ff20a49c0ebd3556b91aa05850e3c
Natalia Fedorova <nataliafedorova1989@gmail.com>
2022-05-17 17:00:08 +0200

    add projects content

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,6 +1,7 @@
 // Project files
 import JSONProjects from "../data/projects.json";
 import JSONKthProjects from "../data/projects-kth.json";
+import JSONSaltProjects from "../data/projects-salt.json";
 import ProjectItem from "../components/ProjectItem";
 import ProjectModal from "../components/ProjectModal";
 
@@ -14,6 +15,9 @@ export default function Projects({ setModal }) {
     <ProjectItem key={item.id} item={item} onClick={() => onProject(item)} />
   ));
 
+  const SaltProjectsList = JSONSaltProjects.map((item) => (
+    <ProjectItem key={item.id} item={item} onClick={() => onProject(item)} />
+  ));
   // Methods
   function onProject(item) {
     setModal(<ProjectModal item={item} />);
@@ -26,7 +30,7 @@ export default function Projects({ setModal }) {
       <div className="content-grid">
         <div className="grid-left">
           <p>
-            These are the projects I developed during Software Development Academy at KTH - Royal Instutite of Technology. One individual project and 2 fullstack group projects.
+            These are the projects I developed during Software Development Academy at KTH - Royal Instutite of Technology in Stockholm. One individual project and 2 fullstack group projects.
           </p>
         </div>
         <div className="grid-left" id="projects-section">
@@ -37,11 +41,22 @@ export default function Projects({ setModal }) {
       <div className="content-grid">
         <div className="grid-left">
           <p>
-            I worked on following projects during the Frontend Development course at Novare Potential. All of these are my invidiual projects.
+            I worked on following projects during the Frontend Development training at Novare Potential in Stockholm. All of these are my invidiual projects.
           </p>
         </div>
         <div className="grid-left" id="projects-section">
           {ProjectsList}
+        </div>
+      </div>
+      <h3>School of Applied Technology</h3>
+      <div className="content-grid">
+        <div className="grid-left">
+          <p>
+            I developed these projects during the Full Stack JavaScript Bootcamp at SALT. Some projects were completed in teams and some - individually.
+          </p>
+        </div>
+        <div className="grid-left" id="projects-section">
+          {SaltProjectsList}
         </div>
       </div>
     </div>
